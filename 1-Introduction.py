515dda7b3933b46aec92f51da9e3ba262e966060
PiroManus <sahunitesh921@gmail.com>
2025-09-27 18:56:28 +0530

    commit3

--- 1-Introduction.py
+++ 1-Introduction.py
@@ -211,3 +211,205 @@ elif location2 == location:
     elif not(temp < 0 or temp > 30):
         print("The temperature is good today!")
         print("Go outside!")
+
+
+#Lesson 11: While loops = a statement that will execute
+#its block of code, as long as its condition remains true.
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+#will try to create a loop for user to enter name.
+# need an escape to the while loop
+
+#To start a code execution below, Ctrl + F5
+#To stop a code execution below, Ctrl + F2
+
+name = ""
+while len(name) == 0:
+    name = input("Enter your name: ")
+print("Hello "+name)
+
+#another way to write this:
+name = None
+while not name:
+    name = input("Enter your name: ")
+print("Hello "+name)
+
+
+#Lesson 12: For Loops
+# a statement that will execute its block of code a limited
+# amount of times.
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+import time
+
+#i means index.
+# The +1 will make the count 1 to 10 because starts at 0.
+# Can also add the +1 to the range
+#for i in range(10):
+    #print(i+1)
+
+#this program counts by 2
+#for i in range(50,100+1,2):
+    #print(i)
+
+#for i in "Bro Code":
+    #print(i)
+
+#import time module at top of code.
+# i can be anything. below it is seconds.
+# the -1 it the step amount. so here it is counting down.
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
+
+
+#Lesson 13: Nested Loops
+# the "inner loop" will finish all its interations before
+# finishing on iteration of the "outer loop"
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+#going to draw a rectange with nested loops.
+#Outer loop will be in charge of rows. Inner incharge of columns.
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
+#common convetion for innter for loops is j.
+#end="" prevents cursor from moving to next line.
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
+
+#Lesson 14: Loop Control Statements
+# Change a loop execution from its normal sequence
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+# break = used to terminate the loop entirely
+# continue = skips to the next ieration of the loop
+# pass = dows nothing, acts as a placeholder
+
+#while True:
+    #name = input("Enter your name: ")
+    #if name !="":
+        #break
+
+phone_number = "123-456-7890"
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="")
+
+for i in range(1,21):
+    if i == 13:
+        pass
+    else: print(i)
+
+
+#Lesson 15: Lists
+# used to store multiple items in a single variable.
+# element is a part of the list.
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+food = ["pizza", "hamburger", "hotdog"]
+food.append("ice cream")
+food.remove("hotdog")
+food.pop() #removes last element in list
+food.insert(0,"cake")
+food.sort()
+food.clear()
+
+#print(food[0])
+for x in food:
+    print(x)
+
+
+#Lesson 16: 2D Lists
+# a lists of lists
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+desert = ["cake", "ice cream"]
+
+food = [drinks, dinner, desert]
+print(food[0][1])
+print(food[1][1])
+
+
+#Lesson 17: Tuple
+# a collection which is ordered and unchangeable. used to group together realted data
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+student = ("Bro", 21, "male")
+print(student.count("Bro"))
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if "Bro" in student:
+    print("Bro is here!")
+
+
+#Lesson 18: Sets
+# collection which is undordered, unindexed. no duplicate values.
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+utensils.add("napkin")
+utensils.remove("fork")
+utensils.clear()
+dishes.update(utensils)
+dinner_table = utensils.union(dishes)
+
+print(dishes.difference(utensils))
+print(utensils.intersection(dishes))
+
+for x in dinner_table:
+    print(x)
+
+
+#Lesson 19: Dictionaries
+# A changeable, unordered collection of unique key:value pairs.
+# Fast because they use hashing, allow us to access a value quickly.
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+capitals = {'USA':'Washington DC',
+            'India':'New Dehli',
+            'China': 'Beijing',
+            'Russia': 'Moscow'}
+print(capitals['Russia']) #this causes issues if not in dictionary. Get method better.
+print(capitals.get('Germany')) # this will return None if not there.
+print(capitals.keys()) # will print only the keys
+print(capitals.values()) # will print only the values
+print(capitals.items()) # will print all items
+
+for key, value in capitals.items(): # this will also print whole dictionary.
+    print(key, value)
+
+capitals.update({'Germany': 'Berlin'}) # way to add to dictionary.
+capitals.update({'USA': 'Las Vegas'}) # way to update or add to dictionary.
+capitals.pop('China') # removes key, value from dictionary.
+capitals.clear()
+
+
+#Lesson 20: Index operator []
+# gives access to a sequence's element (str, list, tuples)
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+name = "bro Code!"
+if(name[0].islower()):
+    name = name.capitalize()
+print(name)
+
+first_name = name[:3].upper()
+last_name = name[4:].lower()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
+
+
