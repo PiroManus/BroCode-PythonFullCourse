73d06a127e99f0e147774726476455d64bc56547
PiroManus <sahunitesh921@gmail.com>
2025-09-27 18:53:26 +0530

    commit 1

--- 1-Introduction.py
+++ 1-Introduction.py
@@ -4,3 +4,210 @@ print('I love learning Python')
 print("It's really fun")
 
 
+#Lesson 2: Variables
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+#variable = a container for a value.
+# Behaves as the value that it contains
+name = "Bro"
+print(type(name))
+print("Hello " + name)
+first_name="Bro"
+last_name="Code"
+full_name=first_name + last_name
+print("Hello "+ full_name)
+
+#when assign value of int type not with quotes
+# because cant use strings with math
+age = 21
+age +=1
+#Better way to write above is age +=1
+print(age)
+print(type(age))
+print('Your age is: '+str(age))
+
+#Float
+height=250.5
+print(height)
+print(type(height))
+
+#Boolean. Can only store True or False
+human = False
+print(human)
+print(type(human))
+
+
+#Lesson 3: Multiple Assignment
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+#Multiple assignment = allows us to assign multiple variables
+# at the same time in one line of code.
+
+name ='Bro'
+age = 32
+attractive = True
+print(name, age, attractive)
+
+#multiple assignment show it is easier to assign.
+name, age, attractive= "Bro", 32, True
+print(name, age, attractive)
+
+#also multiple assignment
+Bill = Tina = Steve = 30
+print(Bill)
+
+
+
+#Lesson 4: String Methods
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+# create a new .py file is Ctrl + Alt + Insert
+name = "Bro"
+#print(len(name))
+
+#this looks for index position of B
+print(name.find("B"))
+
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+#isalpha to check if only alphetical letters
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o", 'a'))
+print(name*3)
+
+
+#Lesson 5: Type Cast
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+# type casting = convert the data type of a value
+# to another data type.
+
+x = 1     #int
+y = 2.0   #float
+z = '3'   #str
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(y)
+print(z*3)
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z*3)
+
+
+#Lesson 6: User Input
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+
+print("Hello "+name)
+print("You are "+str(age)+ " years old")
+print("You are "+str(height)+ " feet tall")
+
+
+#Lesson 7: Math Functions
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+import math
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2)) #raises it to power of 2
+print(math.sqrt(pi)) #square root
+print(max(pi,x,y,z)) #finds max value
+print(min(pi,x,y,z)) #finds min value
+
+
+#Lesson 8: String Slicing
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+# slicing = create a substring by extracing elements from
+# another string. indexing[] or slice()
+# [start:stop:step]
+# start index in inclusive. stop index is exclusive.
+# step is how much increasing index. default is 1.
+
+name = "Bro Code"
+first_name = name[:3]
+last_name = name[4:]
+funky_name = name[::3]
+reversed_name = name[::-1]
+print(first_name)
+print(last_name)
+print(funky_name)
+
+website1 = 'http://test.com'
+website2 = 'http://google.com'
+slice = slice(7,-4)
+print(website1[slice])
+print(website2[slice])
+
+
+#Lesson 9: If Statements
+#a block of code that will execute if it's condition is true
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+age = int(input("How old are you?:"))
+
+if age == 100:
+    print("You are a century old!")
+elif age >= 18:
+    print("You are an adult!")
+elif age <0:
+    print("You have not been born yet!")
+else:
+    print("You are a child!")
+
+
+#Lesson 10: Logical Operators (and, or, not)
+#used to check if two or more conditional statements
+# https://www.youtube.com/watch?v=XKHEtdqhLK8
+#I cannot get this to work.
+#I want to have it be that the user picks where they live to give answer based on cel vs far
+
+location = input("Do you live in the USA? (Yes or No):")
+temp = int(input("What is the temperature outside?:"))
+
+if location == "Yes" or "yes" or "yea" or "Yea":
+    location1 = location
+else: location2 = location
+
+
+if location1 == location:
+    if temp >= 35 and temp <= 90:
+        print("The temperature is good today!")
+        print("Go outside!")
+    elif temp < 35 or temp > 90:
+        print("The temperature is bad today!")
+        print("Stay inside!")
+
+elif location2 == location:
+    if temp >= 0 and temp <= 30:
+        print("The temperature is good today!")
+        print("Go outside!")
+    elif temp < 0 or temp > 30:
+        print("The temperature is bad today!")
+        print("Stay inside!")
+
+
+# not operator: flips it from true to false or reverse
+    if not(temp >= 0 and temp <= 30):
+        print("The temperature is bad today!")
+        print("Stay inside!")
+    elif not(temp < 0 or temp > 30):
+        print("The temperature is good today!")
+        print("Go outside!")
