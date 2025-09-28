69ee381c0632ba7698ce86dfcbf1fcf4b0c958f0
PiroManus <sahunitesh921@gmail.com>
2025-09-28 13:39:59 +0530

    huge commit changes

--- 1-Introduction.py
+++ 1-Introduction.py
@@ -2305,3 +2305,67 @@ myimage = canvas.create_image(0,0, image=photoimage, anchor=NW)
 window.mainloop()
 
 
+#Lesson 91: Animations
+# https://www.youtube.com/watch?v=xiUTqnI6xk8
+from tkinter import *
+import time
+
+WIDTH = 500     #make it upercase to show that it is constant (naming convention)
+HEIGHT = 500
+x_velocity = 3
+y_velocity = 2
+
+window = Tk()
+
+canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+canvas.pack()
+#if want background photo. do this first so photo is in background
+#background_image = PhotoImage(file='person.PNG')
+#background = canvas.create_image(0,0,image=background_image,anchor = NW)
+
+photo_image = PhotoImage(file='person.PNG')
+my_image = canvas.create_image(0,0,image=photo_image,anchor = NW)
+
+image_width = photo_image.width()
+image_height = photo_image.height()
+
+while True:
+    coordinates = canvas.coords(my_image)
+    print(coordinates) # useful for learning purposes
+    if(coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0):
+        x_velocity = -x_velocity #this inverts direction
+    if (coordinates[1] >=(HEIGHT - image_height) or coordinates[1] < 0):
+        y_velocity = -y_velocity  # this inverts direction
+    canvas.move(my_image,x_velocity,0)
+    window.update()
+    time.sleep(0.01)
+
+window.mainloop()
+
+
+#Lesson 92: Multiple Animations
+# https://www.youtube.com/watch?v=xiUTqnI6xk8
+
+from tkinter import *
+from Ball import *   #or Ball
+import time
+
+window = Tk()
+WIDTH = 500
+HEIGHT = 500
+
+canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
+volley_ball = Ball(canvas, 0,0,100,1,1,"white")
+tennis_ball = Ball(canvas, 0,0,50,4,3,"yellow")
+basket_ball = Ball(canvas, 0,0,125,8,7,"orange")
+
+while True:
+    volley_ball.move()
+    tennis_ball.move()
+    basket_ball.move()
+    window.update()
+    time.sleep(0.02)
+
+window.mainloop()
